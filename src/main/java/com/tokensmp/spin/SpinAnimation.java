bd373698267e35b4982da0af307088c9418ccc60
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-15 22:51:25 +0530

    Implement spin effect in SpinAnimation class
    Added a method to play a spin effect with particles and sound.

--- src/main/java/com/tokensmp/spin/SpinAnimation.java
+++ src/main/java/com/tokensmp/spin/SpinAnimation.java
@@ -1,3 +1,35 @@
 package com.tokensmp.spin;
-import com.tokensmp.token.TokenType; import org.bukkit.entity.Player;
-public class SpinAnimation { public void play(Player player, TokenType result){player.sendMessage("You spun: " + result.name());} }
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import com.tokensmp.TokenSMPPlugin;
+
+public class SpinAnimation {
+    public static void playSpinEffect(Player player, Runnable onFinish) {
+        Location loc = player.getLocation().add(0, 1, 0);
+        new BukkitRunnable() {
+            int ticks = 0;
+            @Override
+            public void run() {
+                if (ticks >= 40) {
+                    player.playSound(loc, Sound.UI_TOAST_CHALLENGE_COMPLETE, 1f, 1f);
+                    onFinish.run();
+                    cancel();
+                    return;
+                }
+                // spinning particles
+                double angle = ticks * Math.PI / 10;
+                double radius = 1.5;
+                double x = Math.cos(angle) * radius;
+                double z = Math.sin(angle) * radius;
+                player.getWorld().spawnParticle(Particle.END_ROD, loc.clone().add(x, 0.5, z), 0, 0, 0, 0, 1);
+                player.getWorld().spawnParticle(Particle.SPELL_WITCH, loc, 5, 0.3, 0.3, 0.3, 0.05);
+                if (ticks % 5 == 0) player.playSound(loc, Sound.BLOCK_NOTE_BLOCK_PLING, 0.5f, 1f);
+                ticks++;
+            }
+        }.runTaskTimer(TokenSMPPlugin.getInstance(), 0L, 1L);
+    }
+}
