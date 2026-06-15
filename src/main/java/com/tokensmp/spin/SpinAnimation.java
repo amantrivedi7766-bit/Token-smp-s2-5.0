a32e699d185538ae42322542ad88229f5677be63
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-15 23:30:03 +0530

    Refactor SpinAnimation to improve particle effects

--- src/main/java/com/tokensmp/spin/SpinAnimation.java
+++ src/main/java/com/tokensmp/spin/SpinAnimation.java
@@ -1,11 +1,11 @@
 package com.tokensmp.spin;
 
+import com.tokensmp.TokenSMPPlugin;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import com.tokensmp.TokenSMPPlugin;
 
 public class SpinAnimation {
     public static void playSpinEffect(Player player, Runnable onFinish) {
@@ -17,16 +17,15 @@ public class SpinAnimation {
                 if (ticks >= 40) {
                     player.playSound(loc, Sound.UI_TOAST_CHALLENGE_COMPLETE, 1f, 1f);
                     onFinish.run();
-                    cancel();
+                    this.cancel();
                     return;
                 }
-                // spinning particles
                 double angle = ticks * Math.PI / 10;
                 double radius = 1.5;
                 double x = Math.cos(angle) * radius;
                 double z = Math.sin(angle) * radius;
                 player.getWorld().spawnParticle(Particle.END_ROD, loc.clone().add(x, 0.5, z), 0, 0, 0, 0, 1);
-                player.getWorld().spawnParticle(Particle.SPELL_WITCH, loc, 5, 0.3, 0.3, 0.3, 0.05);
+                player.getWorld().spawnParticle(Particle.WITCH, loc, 5, 0.3, 0.3, 0.3, 0.05);
                 if (ticks % 5 == 0) player.playSound(loc, Sound.BLOCK_NOTE_BLOCK_PLING, 0.5f, 1f);
                 ticks++;
             }
