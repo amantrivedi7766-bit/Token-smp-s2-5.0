7d1426c27128c75094b7b5a521ee32e3bb9db22e
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-15 22:57:23 +0530

    Implement winEffect method in ParticleUtils
    Added winEffect method to spawn fireworks and happy villager particles at a given location.

--- src/main/java/com/tokensmp/utils/ParticleUtils.java
+++ src/main/java/com/tokensmp/utils/ParticleUtils.java
@@ -1,3 +1,21 @@
 package com.tokensmp.utils;
-import org.bukkit.Particle; import org.bukkit.entity.Player;
-public final class ParticleUtils { private ParticleUtils(){} public static void burst(Player player, Particle particle){player.getWorld().spawnParticle(particle, player.getLocation(), 30, 0.5, 0.8, 0.5, 0.02);} }
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+
+public class ParticleUtils {
+    public static void winEffect(Location loc) {
+        World w = loc.getWorld();
+        for (int i = 0; i < 100; i++) {
+            double x = (Math.random() - 0.5) * 2;
+            double z = (Math.random() - 0.5) * 2;
+            w.spawnParticle(Particle.FIREWORK, loc.clone().add(x, 1, z), 0, 0, 0, 0, 1);
+            w.spawnParticle(Particle.VILLAGER_HAPPY, loc.clone().add(x, 0.5, z), 0, 0, 0, 0, 1);
+        }
+    }
+
+    public static void spinWheel(Location loc) {
+        // Already implemented in SpinAnimation
+    }
+}
