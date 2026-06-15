0568cf89cd8f04e61142d97e7736c9de5c9b3c52
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-15 22:46:25 +0530

    Update TokenSMPPlugin.java

--- src/main/java/com/tokensmp/TokenSMPPlugin.java
+++ src/main/java/com/tokensmp/TokenSMPPlugin.java
@@ -1,23 +1,70 @@
 package com.tokensmp;
 
-import com.tokensmp.abilities.AbilityManager;
 import com.tokensmp.commands.*;
-import com.tokensmp.data.*;
-import com.tokensmp.life.*;
-import com.tokensmp.listeners.*;
+import com.tokensmp.data.DataManager;
+import com.tokensmp.gui.AdminGUI;
+import com.tokensmp.life.LifeManager;
+import com.tokensmp.life.ReviveRecipe;
 import com.tokensmp.spin.SpinManager;
 import com.tokensmp.token.TokenManager;
+import com.tokensmp.abilities.AbilityManager;
+import com.tokensmp.listeners.*;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TokenSMPPlugin extends JavaPlugin {
-    private DataManager dataManager; private TokenManager tokenManager; private AbilityManager abilityManager; private SpinManager spinManager; private LifeManager lifeManager; private BountyManager bountyManager;
-    @Override public void onEnable() {
-        saveDefaultConfig(); saveResource("messages.yml", false);
-        dataManager = new DataManager(this); tokenManager = new TokenManager(dataManager); abilityManager = new AbilityManager(this); spinManager = new SpinManager(this, tokenManager); lifeManager = new LifeManager(this, dataManager); bountyManager = new BountyManager();
-        getCommand("token").setExecutor(new TokenCommand(tokenManager)); getCommand("tokeninfo").setExecutor(new TokenInfoCommand()); getCommand("spin").setExecutor(new SpinCommand(spinManager)); getCommand("tokenadmin").setExecutor(new AdminCommands(this));
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(dataManager), this); getServer().getPluginManager().registerEvents(new PlayerDeathListener(lifeManager, bountyManager), this); getServer().getPluginManager().registerEvents(new ItemUseListener(abilityManager), this);
-        new ReviveRecipe(this).register(); dataManager.load();
+    private static TokenSMPPlugin instance;
+    private DataManager dataManager;
+    private TokenManager tokenManager;
+    private SpinManager spinManager;
+    private LifeManager lifeManager;
+    private AbilityManager abilityManager;
+    private AdminGUI adminGUI;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        saveResource("messages.yml", false);
+
+        dataManager = new DataManager();
+        tokenManager = new TokenManager();
+        spinManager = new SpinManager();
+        lifeManager = new LifeManager();
+        abilityManager = new AbilityManager();
+        adminGUI = new AdminGUI();
+
+        registerCommands();
+        registerEvents();
+        new ReviveRecipe();
+
+        getLogger().info("TokenSMP v" + getDescription().getVersion() + " enabled!");
     }
-    @Override public void onDisable() { if (dataManager != null) dataManager.save(); }
-    public DataManager getDataManager(){return dataManager;} public TokenManager getTokenManager(){return tokenManager;} public AbilityManager getAbilityManager(){return abilityManager;} public SpinManager getSpinManager(){return spinManager;} public LifeManager getLifeManager(){return lifeManager;} public BountyManager getBountyManager(){return bountyManager;}
+
+    @Override
+    public void onDisable() {
+        dataManager.saveAll();
+        getLogger().info("TokenSMP disabled.");
+    }
+
+    private void registerCommands() {
+        getCommand("tokens").setExecutor(new TokenCommand());
+        getCommand("tokeninfo").setExecutor(new TokenInfoCommand());
+        getCommand("spin").setExecutor(new SpinCommand());
+        getCommand("tokensmp").setExecutor(new AdminCommands());
+    }
+
+    private void registerEvents() {
+        Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerDeathListener(), this);
+        Bukkit.getPluginManager().registerEvents(new ItemUseListener(), this);
+    }
+
+    public static TokenSMPPlugin getInstance() { return instance; }
+    public DataManager getDataManager() { return dataManager; }
+    public TokenManager getTokenManager() { return tokenManager; }
+    public SpinManager getSpinManager() { return spinManager; }
+    public LifeManager getLifeManager() { return lifeManager; }
+    public AbilityManager getAbilityManager() { return abilityManager; }
+    public AdminGUI getAdminGUI() { return adminGUI; }
 }
