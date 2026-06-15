d1e6103c3bde34d43aa73a1cb039f466bb3756d8
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-15 22:57:55 +0530

    Refactor SoundUtils to add new sound methods

--- src/main/java/com/tokensmp/utils/SoundUtils.java
+++ src/main/java/com/tokensmp/utils/SoundUtils.java
@@ -1,3 +1,15 @@
 package com.tokensmp.utils;
-import org.bukkit.Sound; import org.bukkit.entity.Player;
-public final class SoundUtils { private SoundUtils(){} public static void play(Player player, Sound sound){player.playSound(player.getLocation(), sound, 1.0f, 1.0f);} }
+
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+public class SoundUtils {
+    public static void playWinSound(Player p) {
+        p.playSound(p.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1f, 1f);
+    }
+
+    public static void playSpinStart(Player p) {
+        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1f, 1.2f);
+    }
+}
